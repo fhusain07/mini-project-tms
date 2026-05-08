2796f3d9bed03ceba04b8fd5771f22990947da92
fhusain07 <f.husain07@gmail.com>
2026-05-08 23:53:44 +0530

    changes

--- frontend/src/pages/GenerateToken.jsx
+++ frontend/src/pages/GenerateToken.jsx
@@ -44,7 +44,7 @@ function GenerateToken() {
               type="text"
               value={name}
               onChange={(e) => setName(e.target.value)}
-              placeholder="e.g. Shahzad Ahmad"
+              placeholder="e.g. Natasha Chadekar"
               autoFocus
             />
 
